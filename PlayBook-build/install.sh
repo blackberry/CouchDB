264cc59751013983042be4f7cb9aed52392a65c6
Clifford Hung <chung@rim.com>
2011-11-30 21:26:51 -0500

    Streamline the build script and output an installer.
    - Remove unnecessary compilation of NSPR, CURL and ICU projects.
    - Add getopt and gettext projects to build the getopt binary needed by
    the CouchDB launcher script.
    - Create an installer for CouchDB, Erlang and SpiderMonkey that is run
    on the PlayBook.
    - Update to work with Native SDK 2.0.

---
+++ PlayBook-build/install.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+. ./install-vars.sh
+: ${BUILD_ROOT:?"Error: BUILD_ROOT variable is not set."}
+: ${ERL_DIR:?"Error: ERL_DIR variable is not set."}
+
+PREFIX=`pwd`
+INSTALL_PREFIX=$PREFIX
+ERLANG_DIR=$PREFIX/Erlang
+
+fix_paths()
+{
+    CURDIR=`pwd`
+    FILE_DIR=`dirname $1`
+    FILE_NAME=`basename $1`
+
+    cd $FILE_DIR
+    cat $FILE_NAME | sed -e "s|$BUILD_ROOT|$INSTALL_PREFIX|g" -e "s|$ERL_DIR/bootstrap|$ERLANG_DIR|g" > $FILE_NAME.tmp
+    mv $FILE_NAME.tmp $FILE_NAME
+    cd $CURDIR
+}
+
+# Fix scripts
+cd $PREFIX/CouchDB
+fix_paths ./bin/couchdb
+fix_paths ./bin/couchjs
+fix_paths ./etc/couchdb/default.ini
+fix_paths ./etc/logrotate.d/couchdb
+fix_paths ./etc/init.d/couchdb
+fix_paths ./lib/couchdb/bin/couchjs
+fix_paths ./lib/couchdb/erlang/lib/couch-1.1.0/ebin/couch.app
+
+chmod +x ./lib/couchdb/bin/couchjs
+chmod +x ./bin/couchjs
+chmod +x ./bin/couchdb
+
+# Setup Erlang
+cd $PREFIX/Erlang
+if [ -f bin/run_tests ] ; then
+	rm -f bin/run_tests
+fi
+./Install -minimal $PREFIX/Erlang
+
+mount -uw /base
+
+cd $PREFIX/lib
+cp libjs.so libmozjs.so /usr/lib
+
+cd $PREFIX/bin
+cp getopt /usr/bin
+
+# Start CouchDB
+cd $PREFIX/CouchDB/bin
+echo "Start CouchDB in $PREFIX/CouchDB/bin by running \"./couchdb\"."
+#PATH=$PATH:$PREFIX/Erlang/erts-5.8.4/bin ./couchdb # Set path to inet_gethost
